80b409769d6369de4ce225f461994c4e2a5b9e19
Bhavik Kothari <bkkothari2255@users.noreply.github.com>
2026-01-04 11:07:54 +0530

    Initial commit - GitHub User Finder

---
+++ script.js
@@ -0,0 +1,38 @@
+const username = document.querySelector("#username");
+const button = document.querySelector("#search-btn");
+const profile = document.querySelector("#profile");
+
+async function fetchUser(user) {
+   try{
+        const response = await fetch(`https://api.github.com/users/${user}`);
+        const data = await response.json();
+        if (data.message === "Not Found") {
+            profile.innerHTML = `<p class="error">User not found!</p>`;
+            return;
+        }
+        profile.innerHTML = `
+        <img src="${data.avatar_url}" alt="Avatar" class="avatar">
+        <h3>${data.name || data.login}</h3> 
+        <p>${data.bio || "No bio available."}</p>
+        <a href="${data.html_url}" target="_blank">View Profile</a>
+        `;
+
+        username.value = "";
+   }
+   catch(error){
+        console.error("Error fetching user data:", error);
+   }
+}
+
+button.addEventListener("click", () => {
+    const user = username.value.trim();
+    if(user) {
+        fetchUser(user);
+    }
+});
+
+username.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        button.click(); 
+    }
+});
